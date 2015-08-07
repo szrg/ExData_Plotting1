eee1d6fe22d37976260a0f4a6fb34e6bf9cd8711
szrg <szrg@hotmail.sk>
2015-08-07 12:59:52 +0200

    Legend test adjusted left.

--- plot3.R
+++ plot3.R
@@ -39,7 +39,8 @@ with(data, lines(datetime, sub_metering_1, col='black'))
 with(data, lines(datetime, sub_metering_2, col='red'))
 with(data, lines(datetime, sub_metering_3, col='blue'))
 
-legend('topright', 
+legend('topright',
+       adj=0.05,
        c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
        col=c('black','red', 'blue'), 
        lty=c(1, 1, 1), 
